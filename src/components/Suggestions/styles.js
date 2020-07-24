b17d43a2da90f1531b12bf8f462bd7393c925654
Fernand <fernando.vgarcia@hotmail.com>
2020-07-23 21:23:03 -0300

    Header and suggestion section done.

---
+++ src/components/Suggestions/styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.ScrollView.attrs(() => ({
+    horizontal: true,
+    showsHorizontalScrollIndicator: false,
+    contentContainerStyle: {
+        alignItems: 'center',
+        paddingLeft: 16,
+    }
+}))`
+    background: #1e222b;
+    height: 130px;
+`;
+
+export const Option = styled.TouchableOpacity`
+    width: 80px;
+    margin-right: 16px;
+    align-items: center;
+`;
+
+export const Img = styled.Image``;
+
+export const AreaLabel = styled.View`
+    height: 42px;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Label = styled.Text`
+    color: #fff;
+    font-weight: bold;
+    margin-top: 8px;
+    font-size: 14px;
+    text-align: center;
+`;
